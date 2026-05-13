065c4bb13a08e9a5e63b272b78d2b01759c32014
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-05-13 12:45:13 -0700

    Setup Event Listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import { initializeDom } from './dom';
+import { initializeDom, changeBackground } from './dom';
+import { processData } from './data';
+import { Weather } from './weather';
 
 initializeDom();
+
+function setupEventListeners() {
+  const getLocation = document.querySelector('#getLocation');
+
+  getLocation.addEventListener('keydown', async (e) => {
+    if (e.key === 'Enter') {
+      const data = await processData(getLocation.value);
+      getLocation.value = '';
+      changeBackground(data.getCurrentCondition());
+      console.log(data);
+    }
+  });
+}
+setupEventListeners();
