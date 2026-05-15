cff576d6c824ef7aeb8062f8b48f890d513304bf
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-05-15 13:18:28 -0700

    Update Rendering Logic
    Switch to JSX like syntax from pure dom manipulation.

--- src/dom.js
+++ src/dom.js
@@ -3,129 +3,27 @@ import overcast from './images/overcast.jpg';
 import sunny from './images/sunny.jpg';
 
 function createNavBar() {
-  //create nav container
-  const body = document.querySelector('body');
-  const navContainer = document.createElement('nav');
-  body.appendChild(navContainer);
-
-  //app name
-  const appName = document.createElement('div');
-  appName.id = 'appName';
-  appName.textContent = 'Weather App';
-  navContainer.appendChild(appName);
-
-  //search bar
-  const searchBoxContainer = document.createElement('div');
-  searchBoxContainer.id = 'searchBox';
-  const searchBar = document.createElement('input');
-  searchBar.type = 'text';
-  searchBar.id = 'getLocation';
-  searchBar.name = 'getLocation';
-  searchBar.placeholder = 'Enter Location';
-  searchBoxContainer.appendChild(searchBar);
-  navContainer.appendChild(searchBoxContainer);
-
-  //temperature unit options
-  const tempContainer = document.createElement('div');
-  tempContainer.id = 'unit';
-
-  //celcuis
-  const CelciusBtn = document.createElement('button');
-  CelciusBtn.classList = 'unitBtn';
-  CelciusBtn.textContent = 'C';
-  tempContainer.appendChild(CelciusBtn);
-
-  const divider = document.createElement('span');
-  divider.textContent = '|';
-  tempContainer.appendChild(divider);
-
-  //fahrenheit
-  const fahrenheitBtn = document.createElement('button');
-  fahrenheitBtn.classList = 'unitBtn';
-  fahrenheitBtn.textContent = 'F';
-  tempContainer.appendChild(fahrenheitBtn);
-
-  navContainer.appendChild(tempContainer);
-}
-
-function weatherContainer() {
-  const body = document.querySelector('body');
-  const weatherContainer = document.createElement('div');
-  weatherContainer.id = 'weatherContainer';
-  body.appendChild(weatherContainer);
-
-  //diaplays current feel and weather condition
-  const currentData = document.createElement('div');
-  currentData.id = 'currentData';
-  weatherContainer.appendChild(currentData);
-  const weatherIcon = document.createElement('div');
-  weatherIcon.id = 'icon';
-  currentData.appendChild(weatherIcon);
-  const currentDate = document.createElement('div');
-  currentDate.id = 'currentDate';
-  currentData.appendChild(currentDate);
-  const currentFeel = document.createElement('div');
-  currentFeel.id = 'currentFeel';
-  currentData.appendChild(currentFeel);
-  const weatherDescription = document.createElement('div');
-  weatherDescription.id = 'weatherDescription';
-  currentData.appendChild(weatherDescription);
-  const sunState = document.createElement('div');
-  sunState.id = 'sunState';
-  currentData.appendChild(sunState);
-
-  //contains additional data about weather
-  const moreDetails = document.createElement('div');
-  moreDetails.id = 'moreDetails';
-  weatherContainer.appendChild(moreDetails);
-}
-
-//sets todays date
-function setDate(date) {
-  const currentDate = document.querySelector('#currentDate');
-  currentDate.textContent = `${date}`;
-}
-
-//sets future weather information for the day
-function setDescription(weather) {
-  const description = document.querySelector('#weatherDescription');
-  description.textContent = `${weather}`;
-}
-
-//sets current temperature
-function setFeel(feel) {
-  const currentFeel = document.querySelector('#currentFeel');
-  currentFeel.textContent = `${feel} ˚C`;
-}
-
-//sets weather icon
-function setIcon() {
-  const icon = document.querySelector('#icon');
-  icon.style.backgroundImage = `url(${sunny})`;
-}
-
-//changed app background based on wheather condition
-function setBackground(currentCondition) {
-  const body = document.querySelector('body');
-  if (
-    currentCondition.toLowerCase() == 'cloudy' ||
-    currentCondition.toLowerCase() == 'overcast'
-  ) {
-    const body = document.querySelector('body');
-    body.style.backgroundImage = `url(${overcast})`;
-  }
+  return `<nav>
+      <div id="appName">Weather App</div>
+      <div id="searchBox">
+        <input
+          type="text"
+          id="getLocation"
+          name="getLocation"
+          placeholder="Enter Location"
+        />
+      </div>
+      <div id="unit">
+        <button class="unitBtn">˚C</button>
+        <span>|</span>
+        <button class="unitBtn">˚F</button>
+      </div>
+    </nav>`;
 }
 
 function initializeDom() {
-  createNavBar();
-  weatherContainer();
+  // const body = document.querySelector('body');
+  // body.innerHTML = `${createNavBar()}`;
 }
 
-export {
-  initializeDom,
-  setBackground,
-  setIcon,
-  setDescription,
-  setDate,
-  setFeel,
-};
+export { initializeDom };
