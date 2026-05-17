6195df25c0dd76af1a22b09e8cda95349f784b3e
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-05-17 10:40:30 -0700

    Create Functions for Current and Additional Info

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,7 @@
 function createNavBar() {
   return `<nav>
       <div id="appName">Weather App</div>
+      <div class="currentLocation"></div>
       <div id="searchBox">
         <input
           type="text"
@@ -17,9 +18,75 @@ function createNavBar() {
     </nav>`;
 }
 
+function currentWeather() {
+  return `<div id="currentWeather">
+        <div id="currentHeader">
+          <p>Current Weather</p>
+          <p id="currentDay">Friday</p>
+        </div>
+        <div id="currentTemp">13<span>˚C</span></div>
+        <div id="speedUnit">
+          <select name="speed" id="speed">
+            <option value="miles">Miles</option>
+            <option value="kilometers">Km</option>
+          </select>
+        </div>
+        <div id="feelContainer">
+          <span id="icon"></span>
+          <span
+            ><p id="currentFeel">Feels like</p>
+            <p id="currentDescription">Coludy</p></span
+          >
+        </div>
+      </div>`;
+}
+
+function additionalInformation() {
+  return `<div id="additionalInfo">
+        <div id="rain">
+          <div id="rain-icon"></div>
+          <div>Chance of rain</div>
+          <div id="rain-prob"></div>
+        </div>
+        <div id="humidity">
+          <div id="humidity-icon"></div>
+          <div>Humidity</div>
+          <div id="currentHumidity"></div>
+        </div>
+        <div id="wind">
+          <div id="wind-icon"></div>
+          <div>Wind Speed</div>
+          <div id="currentWindSpeed"></div>
+        </div>
+        <div id="uv">
+          <div id="uv-icon"></div>
+          <div>UV Index</div>
+          <div id="currentUv"></div>
+        </div>
+        <div id="visibility">
+          <div id="visibility-icon"></div>
+          <div>Visibility</div>
+          <div id="currentVisibility"></div>
+        </div>
+        <div id="windDirection">
+          <div id="compass-icon"></div>
+          <div>Wind Direction</div>
+          <div id="currentWindDirection"></div>
+        </div>
+      </div>`;
+}
+
+function weatherContainer() {
+  return `<div id="weatherContainer">
+  ${currentWeather()}
+  ${additionalInformation()}
+  </div>`;
+}
+
 function initializeDom() {
-  // const body = document.querySelector('body');
-  // body.innerHTML = `${createNavBar()}`;
+  const body = document.querySelector('body');
+  body.innerHTML = `${createNavBar()}
+  ${weatherContainer()}`;
 }
 
 export { initializeDom };
