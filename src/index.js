67b2be3cf3dd162bad7a2e527b018a8b734fb7ac
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-05-17 12:01:42 -0700

    Import Setters and Set Data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import { initializeDom } from './dom';
+import {
+  initializeDom,
+  setTemp,
+  setFeel,
+  setDescription,
+  setRainProb,
+  setHumidity,
+  setWindSpeed,
+  setUv,
+  setVisibility,
+  setWindDirection,
+  setRiseTime,
+  setSetTime,
+} from './dom';
 import { processData } from './data';
 import { Weather } from './weather';
 
 initializeDom();
+
+function getElements() {
+  const set = {
+    currentTemp: document.querySelector('#currentTemp'),
+    currentFeel: document.querySelector('#currentFeel'),
+    currentDescription: document.querySelector('#currentDescription'),
+    rainProb: document.querySelector('#rain-prob'),
+    humidity: document.querySelector('#currentHumidity'),
+    windSpeed: document.querySelector('#currentWindSpeed'),
+    uv: document.querySelector('#currentUv'),
+    visibility: document.querySelector('#currentVisibility'),
+    windDir: document.querySelector('#currentWindDirection'),
+    sunRiseTime: document.querySelector('#rise-time'),
+    sunSetTime: document.querySelector('#set-time'),
+  };
+  return set;
+}
 
 function setupEventListeners() {
   const getLocation = document.querySelector('#getLocation');
@@ -11,7 +41,20 @@ function setupEventListeners() {
     if (e.key === 'Enter') {
       const data = await processData(getLocation.value);
       getLocation.value = '';
-      console.log(data);
+      setTemp(getElements().currentTemp, data.getTemp());
+      setFeel(getElements().currentFeel, data.getFeel());
+      setDescription(
+        getElements().currentDescription,
+        data.getCurrentCondition(),
+      );
+      setRainProb(getElements().rainProb, data.getRainProb());
+      setHumidity(getElements().humidity, data.getHumidity());
+      setWindSpeed(getElements().windSpeed, data.getWindSpeed());
+      setUv(getElements().uv, data.getUV());
+      setVisibility(getElements().visibility, data.getVisibility());
+      setWindDirection(getElements().windDir, data.getWindDirection());
+      setRiseTime(getElements().sunRiseTime, data.getSunrise());
+      setSetTime(getElements().sunSetTime, data.getSunset());
     }
   });
 }
