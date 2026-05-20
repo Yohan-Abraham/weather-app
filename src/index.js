852111bf73b1fddb1d37dbab298de70b9a3ed641
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-05-20 11:16:34 -0700

    refactor: Remove Dead Code and import Renderers

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import { initializeDom, renderUIData } from './dom';
-import { processData } from './data';
-import { Weather } from './weather';
-
-function getElements() {
-  const set = {
-    currentTemp: document.querySelector('#currentTemp'),
-    currentFeel: document.querySelector('#currentFeel'),
-    currentDescription: document.querySelector('#currentDescription'),
-    rainProb: document.querySelector('#rain-prob'),
-    humidity: document.querySelector('#currentHumidity'),
-    windSpeed: document.querySelector('#currentWindSpeed'),
-    uv: document.querySelector('#currentUv'),
-    visibility: document.querySelector('#currentVisibility'),
-    windDir: document.querySelector('#currentWindDirection'),
-    sunRiseTime: document.querySelector('#rise-time'),
-    sunSetTime: document.querySelector('#set-time'),
-  };
-  return set;
-}
+import { processData, getForcastedWeather } from './data';
+import { Weather, forcastedWeather } from './weather';
 
 function setupEventListeners() {
   const getLocation = document.querySelector('#getLocation');
@@ -25,8 +8,9 @@ function setupEventListeners() {
   getLocation.addEventListener('keydown', async (e) => {
     if (e.key === 'Enter') {
       const data = await processData(getLocation.value);
+      const forcastedData = await getForcastedWeather(getLocation.value);
       getLocation.value = '';
-      renderUIData(data);
+      renderUIData(data, forcastedData);
     }
   });
 }
